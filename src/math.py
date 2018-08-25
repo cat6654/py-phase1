3597aee83b7ff53f76e585ba00a516e894399759
cat6654 <cat6654@gmail.com>
2018-08-25 11:37:15 +0300

    Add fibonacci function to Math class Add test for fibonacci function

--- src/math.py
+++ src/math.py
@@ -4,3 +4,23 @@ class Math():
             return 'Invalid input'
         else:
             return value1 + value2
+
+    __fib_cache = {}
+    def generate_fibonacci(length):
+        if length in Math.__fib_cache:
+            return Math.__fib_cache[length]
+        else:
+            if length == 0:
+                return 0
+            if length == 1 or length == 2:
+                return 1
+            Math.__fib_cache[length] = Math.generate_fibonacci(length-1) + Math.generate_fibonacci(length-2)
+            return Math.__fib_cache[length]
+
+
+def main():
+    print(Math.generate_fibonacci(10))
+
+
+if __name__ == "__main__":
+    main()
--- test/math-test.py
+++ test/math-test.py
@@ -4,4 +4,11 @@ from src.math import Math
 class MathTest(unittest.TestCase):
     def test_addition(self):
         # Make test fail
-        self.assertEqual(Math.addition(3, 4), 8)
+        self.assertEqual(Math.addition(4, 4), 8)
+
+    def test_fibonacci(self):
+        actual = Math.generate_fibonacci(11)
+        expected = 89
+        self.assertEqual(
+            actual, expected, 'Actual value {} is different from Expected {} for number 11'.format(actual, expected)
+        )
